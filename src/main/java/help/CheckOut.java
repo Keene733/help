7286429622aab4937449fb727c9074281b11c81f
Keene733 <keenekrasi@gmail.com>
2022-11-20 12:15:58 -0600

    added a file CheckOut

---
+++ src/main/java/help/CheckOut.java
@@ -0,0 +1,18 @@
+package help;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class CheckOut extends JFrame {
+	
+	Display info = new Display();
+	panel = new JPanel();
+	
+ public CheckOut() {
+	 this.setSize(500,500);
+	 this.setVisible(true);
+	 
+	 
+	 
+ }
+}
